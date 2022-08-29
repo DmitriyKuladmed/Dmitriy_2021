fa7a8bd9f853ce3803b4774f8e1169c4582fd6c5
Asus <dima.kulaga5@gmail.com>
2022-08-29 15:03:04 +0300

    Start_files

--- Dmitriy_2021/blog/urls.py
+++ Dmitriy_2021/blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("blog/",views.blog_index, name="blog_index"),
+    path("",views.blog_index, name="blog_index"),
     path("blog/<int:pk>/",views.blog_detail, name="blog_detail"),
 ]
